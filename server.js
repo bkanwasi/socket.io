69a3f517459bbb57b1c9500e5205789b221e203b
bkanwasi <bkanwasi21@gmail.com>
2018-07-14 20:04:29 +0530

    update all

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+server.listen(process.env.port || 3001);
+console.log("server running.. 3001")
+app.get('/',function(req,res){
+
+  res.sendFile(__dirname +"/index.html");
+
+});
+
+connections= [];
+users = [];
+ io.sockets.on("connection",function(socket){
+   
+     connections.push(socket);
+    console.log("connected:  %s sockets connected",connections.length);   
+        socket.on('disconnect',function(){
+        connections.splice(connections.indexOf(socket),1);
+        users.splice(users.indexOf(socket.username),1);
+        updateusername();
+        console.log("disconnected:  %s  sockets connected",connections.length);
+      });
+      socket.on('Send Message',function(data){
+          io.sockets.emit('New Message',{msg:data,username:socket.username});
+     });
+
+     //Create new user
+     socket.on('New User',function(data,callback){
+
+        callback(true);
+        socket.username = data;
+        users.push(socket.username)
+        updateusername();
+     });
+
+    
+     function updateusername(){
+
+       io.sockets.emit('Get User',users);
+     }
+
+  });
